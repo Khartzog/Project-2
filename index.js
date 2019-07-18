6d670cb79b1025e047d9e5b839b389a7a27e10d0
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-07-18 15:49:31 -0600

    did stuff

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const { Pool } = require("pg");
+
+const connectionString = process.env.DATABASE_URL || "postgres://postgres:Kh681378.@localhost:5432/mydb";
+
+const pool = new Pool({connectionString: connectionString}); 
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
