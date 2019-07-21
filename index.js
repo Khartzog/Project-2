9428cf4abfb01e4fe802afa892b5d788845fccb6
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-07-20 23:23:46 -0600

    added a lot of stuff

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const path = require('path')
+var session = require('express-session')
+const fetch = require("node-fetch");
+
+//var await = require('asyncawait/await');
+// var async = require('async');
 const PORT = process.env.PORT || 5000
 
 const { Pool } = require("pg");
@@ -22,34 +27,84 @@ app
     res.end();
   })
 
-  // app.get('/viewEntries', function(req, res){
-  //   res.render('project-2/viewEntries.ejs');
-  //   var username = req.query.username;
-  //   console.log("I made it");
-  //   verifyUser(username, function(error, result){
-  //     console.log("back from the database with results: ", result);
-  //     res.json(result);
-  //   })
+  app.get('/mainPage', function(req, res){
+    res.render('project-2/mainPage.ejs');
+    res.end();
+  })
 
-  //   res.end();
-  // })
+  app.get('/createEntry', function(req, res){
+    res.render('project-2/createEntry.ejs');
+    res.end();
+  })
 
-  function verifyUser(username, callback){
+  app.get('/confirmEntry', function(req, res){
+    res.render('project-2/confirmEntry.ejs')
+    var text = req.query.content;
+    makeEntry(text, function(error, result){
+      
+    });
+  })
 
-    var sql = "SELECT username FROM userinfo WHERE username = $1::string";
-    var params = [username];
+  app.get('/viewEntries', function(req, res){
+    var content = ["It rained a bit today, I forgot my umbrella at work.", "I love saturdays, they are the best!"];
+    res.render('project-2/viewEntries.ejs', {content: content});
+    var username = req.query.username;
+    var pass = req.query.password;
+    var verified = "false";
+    verifyUser(username, pass, function(error, result){
+      console.log("back from the database with results: ", result);
+      verified = "true";
+      // res.json(result);
+    })
+
+    console.log("verified: " + verified);
+    var ent = getEntries();
+    console.log("ent: " + ent);
+    res.end();
+  })
+
+  function verifyUser(username, pass, callback){
+
+    var sql = "SELECT username FROM userinfo WHERE username = $1 and pass = $2";
+    var params = [username, pass];
 
     pool.query(sql, params, function(err, result) {
       if (err){
-        // console.log("there was an error with the database");
-        // console.log(err);
+        console.log("there was an error with the database");
+        console.log(err);
         callback(err, null);
       }
-    //   console.log("found DB result: " + JSON.stringify(result.rows));
-
-      callback(null, result.rows);
+        callback(null, result.rows);
 
     })
   };
+
+  function getEntries(){
+    var sql = "SELECT entry_content FROM user_entry WHERE userid = 1";
+    var entries = [];
+   
+    // db.tx(t => {
+    pool.query(sql, function(err, result, done){
+      console.log(result.rows);
+      entries.push(result.rows);
+    })
+    console.log("en: " + entries);
+  };
+
+  function makeEntry(content, callback){
+    var sql = query = "INSERT INTO user_entry(entry_content, entry_date, userid, journal_id) VALUES($1, now(), 1, 1)";
+    var params = [content];
+
+    pool.query(sql, params, function(err, result) {
+      if (err){
+        console.log("there was an error with the database");
+        console.log(err);
+        callback(err, null);
+      }
+        callback(null);
+
+    })
+  };
+
 
   
